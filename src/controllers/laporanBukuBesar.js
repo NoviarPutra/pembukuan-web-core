6bf1a5b4b372bdd243b5258f2ac16e5f14541f9d
Noviar Putra Budiawan <45009698+NoviarPutra@users.noreply.github.com>
2022-10-24 18:35:29 +0700

    laporan

--- src/controllers/laporanBukuBesar.js
+++ src/controllers/laporanBukuBesar.js
@@ -8,23 +8,35 @@ const document = new PDFDocument({ size: "A4", font: "Helvetica" });
 module.exports = {
   getAllLaporan: async (req, res) => {
     try {
+      let buffers = [];
       const fileName = `Laporan-Buku-Besar` + new Date().getTime();
-      const writeStream = fs.createWriteStream(
-        "./src/report/" + fileName + ".pdf"
-      );
-      res.writeHead(200, {
-        "Content-Type": "application/pdf",
-        "Content-disposition": "inline;filename=" + fileName,
-      });
-      document.pipe(writeStream);
-      document.pipe(res);
-      genCommonHeader(document, "Laporan Buku Besar");
-      // document.on("data", (data) => {
-      //   stream.write(data);
+      // const writeStream = fs.createWriteStream(
+      //   "./src/report/" + fileName + ".pdf"
+      // );
+      // res.writeHead(200, {
+      //   "Content-Type": "application/pdf",
+      //   "Content-disposition": "inline;filename=" + fileName,
       // });
-      // document.on("end", () => stream.end());
+      // document.pipe(writeStream);
+      // document.pipe(res);
+      genCommonHeader(document, "Laporan Buku Besar");
+      document.on("data", buffers.push.bind(buffers));
+      document.on(
+        "end",
+        () => {
+          let pdfData = Buffer.concat(buffers);
+          res
+            .writeHead(200, {
+              "Content-Length": Buffer.byteLength(pdfData),
+              "Content-Type": "application/pdf",
+              "Content-disposition": `attachment;filename=${fileName}.pdf`,
+            })
+            .end(pdfData);
+        },
+        (err) => console.log(err)
+      );
       document.end();
-      writeStream.on("error", (err) => console.log(err));
+      // writeStream.on("error", (err) => console.log(err));
     } catch (error) {
       console.log(error);
     }
