57c78b3cf316d3cec8f33dcdda136b8f0f7a9002
Noviar Putra Budiawan <45009698+NoviarPutra@users.noreply.github.com>
2022-10-27 23:50:50 +0700

    pull

--- src/controllers/tester.js
+++ src/controllers/tester.js
@@ -111,4 +111,96 @@ module.exports = {
       return res.status(400).json(err400(error));
     }
   },
+  testLabaRugi: async (req, res) => {
+    try {
+      const { tahun } = req.params;
+      // const resp = await Jurnal.aggregate([
+      //   {
+      //     $match: {
+      //       $and: [
+      //         {
+      //           tanggalJurnal: {
+      //             $gte: new Date(`${tahun}-01-01`),
+      //             $lte: new Date(`${tahun}-12-31`),
+      //           },
+      //         },
+      //         { kodePerkiraan: { $gte: "600", $lte: "799" } },
+      //       ],
+      //     },
+      //   },
+      //   {
+      //     $group: {
+      //       _id: {
+      //         tanggalJurnal: "$tanggalJurnal",
+      //         kodePerkiraan: "$kodePerkiraan",
+      //         namaPerkiraan: "$namaPerkiraanJurnal",
+      //       },
+      //       debet: { $sum: "$debet" },
+      //       kredit: { $sum: "$kredit" },
+      //     },
+      //   },
+      // ]).sort({ _id: "asc" });
+      const resp = await Jurnal.aggregate([
+        {
+          $match: {
+            $and: [
+              {
+                tanggalJurnal: {
+                  $gte: new Date(`${tahun}-01-01`),
+                  $lte: new Date(`${tahun}-12-31`),
+                },
+              },
+              { kodePerkiraan: { $gte: "600", $lte: "799" } },
+            ],
+          },
+        },
+        {
+          $group: {
+            _id: {
+              tanggalJurnal: "$tanggalJurnal",
+              kodePerkiraan: "$kodePerkiraan",
+              namaPerkiraan: "$namaPerkiraanJurnal",
+            },
+            debet: { $sum: "$debet" },
+            kredit: { $sum: "$kredit" },
+          },
+        },
+      ]).sort({ _id: "asc" });
+      if (!resp[0]) return res.status(404).json(err404());
+
+      const total = await Jurnal.aggregate([
+        {
+          $match: {
+            $and: [
+              {
+                tanggalJurnal: {
+                  $gte: new Date(`${tahun}-01-01`),
+                  $lte: new Date(`${tahun}-12-31`),
+                },
+              },
+              { kodePerkiraan: { $gte: "600", $lte: "799" } },
+            ],
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            debet: { $sum: "$debet" },
+            kredit: { $sum: "$kredit" },
+          },
+        },
+      ]);
+      return res.status(200).json({
+        code: 200,
+        status: "OK",
+        totalDebet: total[0].debet,
+        totalKredit: total[0].kredit,
+        totalSaldo: total[0].debet - total[0].kredit,
+        data: resp,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json(err400(error));
+    }
+  },
 };
