a4b5ca4b88ccf020e9319ed7d3774e94ff6def01
RohotSilaban23 <rohotsilaban9@gmail.com>
2022-10-28 11:47:17 +0700

    mau pull main

--- src/models/schema.js
+++ src/models/schema.js
@@ -29,38 +29,15 @@ const JurnalUmunSchema = Schema(
   { versionKey: false }
 );
 
-const LabaRugiSchema = Schema({
-  tanggalLabaRugi: { type: Date },
-  kodePerkiraan: [JurnalUmunSchema],
-  lbDebet: { type: Number },
-  lbKredit: { type: Number },
-});
 
-const ArusKasSchema = Schema(
-  {
-    nomerJurnal: { type: Number, required: true },
-    tanggalJurnal: Date,
-    uraian: { type: String, required: true },
-    nomerBukti: { type: String, require: true },
-    kodePerkiraan: { type: String, required: true },
-    namaPerkiraanJurnal: { type: String, required: true },
-    debet: { type: Number, required: true, default: 0 },
-    kredit: { type: Number, required: true, default: 0 },
-    saldo: { type: Number, required: true, default: 0 },
-  },
-  { versionKey: false }
-);
 
 userSchema.set("timestamps", true);
 perkiraanSchema.set("timestamps", true);
 JurnalUmunSchema.set("timestamps", true);
-LabaRugiSchema.set("timestamps", true);
-ArusKasSchema.set("timestamps", true);
+
 
 const User = mongoose.model("User", userSchema);
 const Perkiraan = mongoose.model("Perkiraan", perkiraanSchema);
 const Jurnal = mongoose.model("jurnalUmum", JurnalUmunSchema);
-const Labarugi = mongoose.model("Labarugi", LabaRugiSchema);
-const Aruskas = mongoose.model("arusKas", ArusKasSchema);
 
 module.exports = { User, Perkiraan, Jurnal, Labarugi, Aruskas };
