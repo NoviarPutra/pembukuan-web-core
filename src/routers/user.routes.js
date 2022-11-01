1227a40c0f900488f2045dd024b076e139f48dd8
Noviar Putra Budiawan <45009698+NoviarPutra@users.noreply.github.com>
2022-11-01 19:33:17 +0700

    auth User

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -10,13 +10,15 @@ const {
   validateSignUp,
   validateSignIn,
   validateUpdateUser,
+  authToken,
+  isAdmin,
 } = require("../middlewares");
 const router = express.Router();
 
 router.get("/", getAllUsers);
 router.post("/signup", [validateSignUp], signUp);
 router.post("/signin", [validateSignIn], signIn);
-router.put("/:id", [validateUpdateUser], updateUser);
-router.delete("/delete/:id", removeUser);
+router.put("/:id", [authToken, isAdmin, validateUpdateUser], updateUser);
+router.delete("/delete/:id", [authToken, isAdmin], removeUser);
 
 module.exports = router;
